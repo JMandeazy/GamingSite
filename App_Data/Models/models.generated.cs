95386210d27dd067e37962b766603fa34d4d8127
Joakim <Joakiimm@hotmail.com>
2019-11-07 14:41:24 +0100

    added product-info pages

--- App_Data/Models/models.generated.cs
+++ App_Data/Models/models.generated.cs
@@ -19,8 +19,8 @@ using Umbraco.ModelsBuilder;
 using Umbraco.ModelsBuilder.Umbraco;
 
 [assembly: PureLiveAssembly]
-[assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "6eb65bb3e8c47d5a")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
+[assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "46784b0c9ccd25a8")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 namespace Umbraco.Web.PublishedModels
 {
@@ -50,11 +50,11 @@ namespace Umbraco.Web.PublishedModels
 		// properties
 
 		///<summary>
-		/// ListedProducts
+		/// FeaturedProducts
 		///</summary>
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder", "8.1.0")]
-		[ImplementPropertyType("listedProducts")]
-		public IEnumerable<IPublishedContent> ListedProducts => this.Value<IEnumerable<IPublishedContent>>("listedProducts");
+		[ImplementPropertyType("feature")]
+		public IEnumerable<IPublishedContent> Feature => this.Value<IEnumerable<IPublishedContent>>("feature");
 
 		///<summary>
 		/// Products
@@ -101,7 +101,7 @@ namespace Umbraco.Web.PublishedModels
 		///</summary>
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder", "8.1.0")]
 		[ImplementPropertyType("productImage")]
-		public IPublishedContent ProductImage => this.Value<IPublishedContent>("productImage");
+		public string ProductImage => this.Value<string>("productImage");
 
 		///<summary>
 		/// Product Name
---
+++ javascript/shoppingcart.js
@@ -0,0 +1,28 @@
+function getFromLocalStorage() {
+  let productsLS = getProductsFromStorage();
+  //loop through the products and print into the cart
+  productsLS.forEach(function (product) {
+    //create the <ntr>
+    totalPrice += Number(product.price);
+    const row = document.createElement("tr");
+    console.log(product.price);
+    //print the contents
+    row.innerHTML = `
+            <tr>
+                <td>
+                  <img src="${product.image}" width=100>
+                </td>
+                <td>${product.title}</td>
+                <td class="product-price" data-price="${product.price}">${
+      product.price
+      }</td>
+                <td>
+                <a href="#" class="remove" data-id="${product.id}">X</a>
+                </td>
+             </tr>
+            `;
+    shoppingCartContent.appendChild(row);
+  });
+  console.log(totalPrice);
+  totalHTML.innerHTML = totalPrice;
+}
